0d9bad23eb1b24ec830b180942065a84b568da2e
Chuck <chuckcipione@yahoo.com>
2017-08-29 20:40:39 -0500

    objects

--- objects.js
+++ objects.js
@@ -1,43 +1,52 @@
 // Create an object called user that has name, age, password properties and assign them appropriate values.
 
-// Code Here
-
+user = {
+    name: 'Chuck',
+    age: 26,
+    password: 'passw0rd'
+};
 
 
 // Add an email and username, as well as a method called alertUser that alerts the users name, to your user object using dot notation.
 
-// Code Here
+user.email = "chuckcipione@yahoo.com";
+user.username = "chuckcip";
 
+alert (JSON.stringify(user));
 
 
 // Now loop over your user object and alert each of the keys.
-
-// Code Here
+for (var key in user){
+    alert (JSON.stringify(key));
+}
 
 
 
 // Now loop over your user object and alert each of the values for each key.
-
-// Code Here
+for (var key in user){
+    alert (JSON.stringify(user));
+}
 
 
 
 // Remove the username from your user object
-
-// Code Here
+delete user.username;
 
 
 
 // Create an object called creditCard that has number, cvc, type, and limit properties and assign them appropriate values.
 
-// Code Here
-
+creditCard = {
+    number: 123456,
+    cvc: 123,
+    type: 'MasterCard',
+    limitproperties: 'none'
+}
 
 
 // Create a variable called alternating. Set it equal to the string bonusCategories.
 
-// Code Here
-
+var alternating = "bonusCategories";
 
 
 /* Using bracket notation, add a bonusCategories key to your creditCard object using 'alternating'.
@@ -50,15 +59,14 @@
         the bank name, and the bank address.
     
     */
-
-// Code Here
+creditCard['bonusCategories'] = 'key', '%'
+creditCard['bank'] = "bank name", "bank address"; 
 
 
 
 // Write a function that will take your creditCard object and user object as arguments and combine them into one object. /* HINT: You will need a for..in loop */
 
-// Code Here
-
+Object.assign(user, creditCard);
 
 /*Below you are given an object. Write a function that will loop over the allies property, remove LittleFinger, and insert Dragon Lady.
     It will also change alive to 'More Than Ever'.
@@ -70,6 +78,9 @@ var obj = {
     alive: 'Sort of',
   };
 
-  // Code Here
-
+function games(){
+    for (var i = 0; i < allies.length; i++){
+        allies.splice(3, 1, 'Dragon Lady');
+    } 
+}
   
